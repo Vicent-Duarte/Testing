d952826ac241a96a3a5bcbb7ce234f9dbb8b2577
Vicent-Duarte <vicentduarte70@gmail.com>
2024-08-14 23:52:11 -0400

    rutas dinamicas de testing

--- src/tests/city.test.js
+++ src/tests/city.test.js
@@ -2,19 +2,22 @@ const request = require("supertest");
 const app = require ('../app');
 const supertest = require("supertest");
 
+let cityId = null; // guardamos el id para usarlo en las rutas dinamicas
+const BASE_URL = '/api/v1/cities';
+
 const city = {
     name: "Caracas",
     country: "Venezuela",
     isCapital: true
 };
 
-const BASE_URL = '/api/v1/cities';
 
 test("POST -> BASE_URL, should return statusCode 201, and res.body.name === city.name", async() => {
     const res = await request(app)
     .post(BASE_URL)
     .send(city)
     
+    cityId = res.body.id
     expect(res.status).toBe(201)
     expect(res.body).toBeDefined()
     expect(res.body.name).toBe(city.name)
@@ -28,3 +31,7 @@ test("GET -> BASE_URL, should return statusCode 200, and res.body.length === 1",
     expect(res.body).toBeDefined()
     expect(res.body).toHaveLength(1)
 });
+
+// Rutas dinamicas 
+///api/v1/cities
+//? BASE_URL  Valor= Dinamico
